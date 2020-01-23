64c68d0720a44ee53ef7319f6bea795aa6c9cf9a
Shohin <shohin_6707@mail.ru>
2020-01-23 17:08:31 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Tele2 from './components/tele2/Tele2'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const tarrifs = [
+        {id: 1, title: "Мой онлайн+", price: 700, minutes: 800, text: "на остальные номера Росии", gb: 30 },
+        {id: 2, title: "Везде онлайн", price: 500, minutes: 500, text: "на остальные номера Росии", gb: 40 },
+        {id: 3, title: "Мой онлайн", price: 400, minutes: 500, text: "на другие мобильные номера домашнего региона", gb: 15 }
+    ]
+    return (
+        <div>
+            <Tele2 id={tarrifs[0].id} title={tarrifs[0].title} price={tarrifs[0].price} minutes={tarrifs[0].minutes} text={tarrifs[0].text} gb={tarrifs[0].gb}/>
+            <Tele2 id={tarrifs[1].id} title={tarrifs[1].title} price={tarrifs[1].price} minutes={tarrifs[1].minutes} text={tarrifs[1].text} gb={tarrifs[1].gb}/>
+            <Tele2 id={tarrifs[2].id} title={tarrifs[2].title} price={tarrifs[2].price} minutes={tarrifs[2].minutes} text={tarrifs[2].text} gb={tarrifs[2].gb}/>
+        </div>
+    )
 }
-
-export default App;
---
+++ src/components/tele2/Tele2.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Tele2(props) {
+    return (
+        <div className="tarrifs__block">
+            <p><b>{props.title}</b></p>
+            <p><b>{props.price}</b>р /Месяц</p>
+            <p>Безлимит на Tele2 Росии</p>
+            <p><b>{props.minutes}</b> мин. {props.text}</p>
+            <p><b>{props.gb}</b> ГБ интернета</p>
+        </div>
+    )
+}
